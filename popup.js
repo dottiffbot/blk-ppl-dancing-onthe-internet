9b62e575265f987c803ecb100d3264ef0a3232e9
tuffany <thetifftopor@gmail.com>
2021-09-18 20:50:10 -0400

    mobile scroll

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@
 const openPops = document.querySelectorAll("[data-open]")
 const closePops = document.querySelectorAll("[data-close]")
 const body = document.querySelector("body")
-const entry = document.querySelectorAll(".entry-info")
+const entry = document.querySelectorAll(".entry-container")
 
 for (const pop of openPops){
   pop.addEventListener('click', togglePop, bodyScrollLock.disableBodyScroll(entry))
